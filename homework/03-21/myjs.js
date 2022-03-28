73d947d9655305462401b976e10621f41051bea9
Haoxiang Yu <hay110@ucsd.edu>
2022-03-28 15:58:16 -0400

    edited HW6

--- homework/03-21/myjs.js
+++ homework/03-21/myjs.js
@@ -4,18 +4,18 @@ function main(){
     width = svg.attr("width") - margin,
     height = svg.attr("height") - margin;
 
-    svg.append("text")
-        .attr("transform", "translate(100,0)")
-        .attr("x",50)
-        .attr("y",50)
-        .attr("font-size","24px")
-        .text("population")
+    //svg.append("text")
+        //.attr("transform", "translate(100,0)")
+        //.attr("x",50)
+        //.attr("y",50)
+        //.attr("font-size","24px")
+        //.text("population")
 
     var xScale = d3.scaleBand().range([0,width]).padding(0.4),
         yScale = d3.scaleLinear().range([height,0]);
 
         var g = svg.append("g")
-        .attr("transform", "translate(" + 100 + "," + 100 + ")");
+            .attr("transform", "translate(" + 100 + "," + 100 + ")");
     
         d3.csv("HW6.csv").then( function(data) {
             xScale.domain(data.map(function(d) { return d.Country; }));
@@ -27,7 +27,7 @@ function main(){
             .append("text")
             .attr("y", height - 250)
             .attr("x", width - 100)
-            .attr("text-anchor", "end")
+            .attr("text-anchor", "start")
             .attr("stroke", "black")
             .text("Country");
 
@@ -41,20 +41,20 @@ function main(){
             .attr('stroke', 'black')
             .text('Population in Thousands')
         
-            g.selectAll(".bar")
+        g.selectAll(".bar")
             .data(data)
             .enter().append("rect")
             .attr("class", "bar")
-        .on("mouseover", onMouseOver) // Add listener for event
-        .on("mouseout", onMouseOut)
-            .attr("x", function(d) { return xScale(d.year); })
-            .attr("y", function(d) { return yScale(d.value); })
+            .on("mouseover", onMouseOver) 
+            .on("mouseout", onMouseOut)
+            .attr("x", function(d) { return xScale(d.Country); })
+            .attr("y", function(d) { return yScale(d.Population); })
             .attr("width", xScale.bandwidth())
-        .transition()
-        .ease(d3.easeLinear)
-        .duration(500)
-        .delay(function(d,i){ return i * 50})
-            .attr("height", function(d) { return height - yScale(d.value); });
+            .transition()
+            .ease(d3.easeLinear)
+            .duration(500)
+            .delay(function(d,i){ return i * 50})
+            .attr("height", function(d) { return height - yScale(d.Population); });
        })
           
 
@@ -67,7 +67,7 @@ function main(){
            d3.select('#tooltip')
                .style('left', xPos + 'px')
                .style('top', yPos + 'px')
-               .select('#value').text(i.value)
+               .select('#value').text(i.Population)
            
            d3.select('#tooltip').classed('hidden', false);
    
@@ -82,7 +82,7 @@ function main(){
    
        }
    
-       // Mouseout event handler
+
        function onMouseOut(d, i){
            d3.select(this).attr('class','bar')
            d3.select(this)
